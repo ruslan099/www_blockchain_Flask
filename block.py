cf019ac9d49cdf38a829456dab7ab87837e9ae74
Ruslan Kurbanov <kurbanovru@list.ru>
2022-03-24 20:54:39 +0300

    first commit

---
+++ block.py
@@ -0,0 +1,63 @@
+import json
+import os
+import hashlib
+
+blockchain_dir = os.curdir + '/blockchain/'
+
+def get_files():
+    files = os.listdir(blockchain_dir)
+    for i in range(len(files)):
+        files[i] = int(files[i])
+    return files
+
+
+def get_hash(filename):
+    file = open(blockchain_dir + str(filename), 'rb').read()
+    return hashlib.md5(file).hexdigest()
+
+
+def check_integrity():
+    # 1. Считать хэш предыдущего блока
+    # 2. Заново вычислить хэш предыдущего блока
+    # 3. Сравнить эти хэши
+    files = get_files()
+    files = sorted(files)
+
+    results = []
+    for file in files[1:]:
+        f = open(blockchain_dir + str(file))
+        h = json.load(f)['hash']
+
+        prev_file = (file - 1)
+        actual_hash = get_hash(prev_file)
+
+        if h == actual_hash:
+            res = 'OK'
+        else:
+            res = 'Corrupted'
+        results.append({'block': prev_file, 'result': res})
+    return results
+
+def write_block(name, amount, to_whom, prev_hash=""):
+    files = get_files()
+
+    last_file = sorted(files)[-1]
+    prev_hash = get_hash(str(last_file))
+    filename = str(last_file + 1)
+    
+    data = {
+        "name": name,
+        'amount': amount,
+        'to_whom': to_whom,
+        'hash': prev_hash
+    }
+
+    with open(blockchain_dir + filename, 'w') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    check_integrity()
+
+if __name__ == '__main__':
+    main()
